20a1d664c106434ce6074301aa7ead53db4f8aeb
urielxdov <urieledgar878@gmail.com>
2026-06-02 14:13:11 -0600

    feat: add SessionProvider to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist } from 'next/font/google';
+import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider, themeScript } from '@/app/components/ThemeProvider';
 import './globals.css';
 
@@ -17,7 +18,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
       </head>
       <body className="min-h-full flex flex-col antialiased bg-surface text-ink">
-        <ThemeProvider>{children}</ThemeProvider>
+        <SessionProvider>
+          <ThemeProvider>{children}</ThemeProvider>
+        </SessionProvider>
       </body>
     </html>
   );
